142904d2d3e835be6e30022d613fdc58b37e3801
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2021-11-01 13:32:32 -0700

    Versus contract support

--- rosetta/scripts/mainnet_contracts.go
+++ rosetta/scripts/mainnet_contracts.go
@@ -18,4 +18,36 @@ var mainnetContracts = map[string]string{
 			return vaultRef.balance + FlowSwapPair.getPoolAmounts().token1Amount
 		}
 	`,
+	"d796ff17107bbff6": `
+		import FungibleToken from 0x{{.Params.FungibleToken}}
+		import {{.Token.Type}} from 0x{{.Token.Address}}
+		import Versus from 0xd796ff17107bbff6
+		
+		pub fun main(account: Address): UFix64 {
+		
+			// default flow token
+			let vaultRef = getAccount(account)
+				.getCapability({{.Token.Balance}})
+				.borrow<&{{.Token.Type}}.Vault{FungibleToken.Balance}>()
+				?? panic("Could not borrow Balance reference to the Vault")
+		
+		
+			// sum of all public drops
+			let publicDrop = getAccount(0xd796ff17107bbff6)
+				.getCapability(Versus.CollectionPublicPath)
+				.borrow<&{Versus.PublicDrop}>()
+				?? panic("Could not borrow Balance reference to the PublicDrop")
+		
+			let allStatuses = publicDrop.getAllStatuses()
+		
+			var totalFlow = 0.0
+		
+			for dropId in allStatuses.keys {
+				let dropStatus = allStatuses[dropId]
+				totalFlow = totalFlow + (dropStatus?.uniquePrice ?? 0.0) + (dropStatus?.editionPrice ?? 0.0)
+			}
+		
+			return totalFlow + vaultRef.balance
+		}
+	`,
 }
